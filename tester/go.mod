d5573897a7a967f4347451913cfdba235277aa49
skyflow-Aditya <aditya.sharma@skyflow.com>
2023-04-03 13:38:33 +0530

    added tester

---
+++ tester/go.mod
@@ -0,0 +1,3 @@
+module github.com/skyflow-Aditya/go-beta-test/tester
+
+go 1.19
---
+++ tester/tester.go
@@ -0,0 +1,8 @@
+package tester
+
+import "fmt"
+
+func collect() bool{
+	fmt.Println("inside collect")
+	return true
+}
